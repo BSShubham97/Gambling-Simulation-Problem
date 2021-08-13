fcc066236b5a116d3415854157b36abdb37db56f
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-13 08:45:53 +0530

    USE CASE 1

--- src/com/bridgelabz/GamblingProblem.java
+++ src/com/bridgelabz/GamblingProblem.java
@@ -1,9 +1,15 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class GamblingProblem {
     public static void main(String[] args){
 
         System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
 
+        int stake=100;
+        int bet=stake/100;
+        System.out.println("Stake:"+stake+" $");
+        System.out.println("Bet:"+bet+" $");
     }
 }
