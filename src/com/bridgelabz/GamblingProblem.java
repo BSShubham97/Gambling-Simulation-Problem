91cff59cbb9e6da24512deb522fe1507f14b3d86
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-13 09:56:14 +0530

    USE CASE 2

--- src/com/bridgelabz/GamblingProblem.java
+++ src/com/bridgelabz/GamblingProblem.java
@@ -1,14 +1,24 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class GamblingProblem {
-    public static final int stake = 100;
-    public static final int bet = 1;
+
+    public static final int STAKE = 100;
+    public static final int BET = 1;
+
+    public static void isWinLoss() {
+        int value = (int) (Math.random() * 10 % 2);
+
+        if (value == 1)
+            System.out.println(" WIN 1$ ");
+        else
+            System.out.println(" LOSE 1$ ");
+    }
+
     public static void main(String[] args) {
 
         System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
-
-
+        isWinLoss();
     }
 }
+
+
