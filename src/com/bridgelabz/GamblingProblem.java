633cb98e79b29bb72884cbc4c1a247908cd9587d
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-13 08:33:53 +0530

    Welcome Message

---
+++ src/com/bridgelabz/GamblingProblem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class GamblingProblem {
+    public static void main(String[] args){
+
+        System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
+
+    }
+}
