b780d523807a9a3bbfb39c41f18fffb03544cf7c
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-13 08:58:46 +0530

    UC-1

--- src/com/bridgelabz/GamblingProblem.java
+++ src/com/bridgelabz/GamblingProblem.java
@@ -3,13 +3,11 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class GamblingProblem {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
 
-        int stake=100;
-        int bet=stake/100;
-        System.out.println("Stake:"+stake+" $");
-        System.out.println("Bet:"+bet+" $");
+        int stake = 100;
+        int bet = 1;
     }
 }
